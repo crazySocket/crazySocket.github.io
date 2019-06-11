0303497080df4c2fa221e2b50eb2f50204f2452b
lord.didger <lord.didger@gmail.com>
2019-06-11 22:40:18 +0200

    remove storage

--- storage/software-delivery/hello.nix
+++
@@ -1,6 +0,0 @@
-derivation {
-  system = "x86_64-linux";
-  name = "hello-1.0";
-  builder = ./hello.sh;
-  source = ./hello-1.0.tar.gz;
-}
--- storage/software-delivery/hello.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-PATH=/usr/bin
-tar xf $source
-cd hello-1.0
-./configure --prefix=$out
-make
-make install
